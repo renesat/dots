fbfa2e9ef99719d9d4466325c3c31d39ccf362b7
DarkNoys <s-paradox@ro.ru>
2019-09-10 19:40:26 +0300

    Remove twitter mode

--- dot_config/doom/packages.el
+++ dot_config/doom/packages.el
@@ -7,9 +7,3 @@
 (package! poetry) ; :recipe (:host github :repo "renesat/poetry.el")
 (package! py-yapf)
 (package! py-isort)
-
-;;;;;;;;;;;;;
-;; Twitter ;;
-;;;;;;;;;;;;;
-
-(package! twittering-mode)
