ff9157537496871e57dec32837e28eb2ec2d959e
DarkNoys <s-paradox@ro.ru>
2019-09-16 15:08:01 +0300

    Fix org-brain config
    Replace ```after!``` to ```:after``` key in ```use-package!```

--- dot_config/doom/modules/tools/org-brain/config.el
+++ dot_config/doom/modules/tools/org-brain/config.el
@@ -1,18 +1,18 @@
 ;;; tool/org-brain/config.el -*- lexical-binding: t; -*-
 
 (use-package! org-brain
+  :after org
   :init
   (setq org-brain-path (concat org-directory
-                               "brain"))
+                               "brain/"))
   (with-eval-after-load 'evil
     (evil-set-initial-state 'org-brain-visualize-mode 'emacs))
   :config
   (setq org-id-track-globally t)
   ;; (setq org-id-locations-file "~/.emacs.d/.org-id-locations")
-  (after! org
-    (push '("b" "Brain" plain (function org-brain-goto-end)
-            "* %i%?" :empty-lines 1)
-          org-capture-templates))
+  (push '("b" "Brain" plain (function org-brain-goto-end)
+          "* %i%?" :empty-lines 1)
+        org-capture-templates)
   (setq org-brain-visualize-default-choices 'all)
   (setq org-brain-title-max-length 100)
   (setq org-brain-include-file-entries nil
