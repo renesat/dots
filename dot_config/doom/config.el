260f63d35b10e512056185deb766727efec74307
DarkNoys <s-paradox@ro.ru>
2019-10-04 00:27:59 +0300

    Xelatex preview in org-mode

--- dot_config/doom/config.el
+++ dot_config/doom/config.el
@@ -7,7 +7,6 @@
 ;;;;;;;;;;;;;;;;;;;;;;
 ;; General settings ;;
 ;;;;;;;;;;;;;;;;;;;;;;
-
 (set-frame-parameter (selected-frame) 'alpha '(90 . 90))
 
 ;; Keys
@@ -44,6 +43,37 @@
 ;;;;;;;;;
 
 (after! org
+  (setq org-preview-latex-default-process 'dvisvgm)
+  (setq org-preview-latex-process-alist
+        '((dvipng :programs
+                  ("xelatex" "dvipng")
+                  :description "dvi > png" :message "you need to install the programs: latex and dvipng."
+                  :image-input-type "xdv" :image-output-type "png" :image-size-adjust
+                  (1.0 . 1.0)
+                  :latex-compiler
+                  ("xelatex -no-pdf -interaction nonstopmode -output-directory %o %f")
+                  :image-converter
+                  ("dvipng -D %D -T tight -o %O %f"))
+          (dvisvgm :programs
+                   ("xelatex" "dvisvgm")
+                   :description "dvi > svg"
+                   :message "you need to install the programs: latex and dvisvgm."
+                   :image-input-type "xdv"
+                   :image-output-type "svg"
+                   :image-size-adjust (1.7 . 1.5)
+                   :latex-compiler
+                   ("xelatex -no-pdf -interaction nonstopmode -output-directory %o %f")
+                   :image-converter
+                   ("dvisvgm %f -n -b min -c %S -o %O"))
+          (imagemagick :programs
+                       ("xelatex" "convert")
+                       :description "pdf > png" :message "you need to install the programs: latex and imagemagick." :image-input-type "pdf" :image-output-type "png" :image-size-adjust
+                       (1.0 . 1.0)
+                       :latex-compiler
+                       ("xelatex -interaction nonstopmode -output-directory %o %f")
+                       :image-converter
+                       ("convert -density %D -trim -antialias %f -quality 100 %O"))))
+
   ;; Src blocks
   (map! :map org-mode-map
         :localleader
