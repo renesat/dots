2a36546cb264b997633a5a45fdce02ab17468886
renesat <s-paradox@ro.ru>
2020-04-08 22:22:52 +0300

    Add ob-julia fork and fix base16-theme

--- dot_config/doom/packages.el
+++ dot_config/doom/packages.el
@@ -3,9 +3,24 @@
 
 ;;; Code:
 
+;;;;;;;;;;;
+;; Theme ;;
+;;;;;;;;;;;
+
+(package! base16-theme)
+
 ;;;;;;;;;;;;
 ;; Python ;;
 ;;;;;;;;;;;;
 (package! poetry) ; :recipe (:host github :repo "renesat/poetry.el")
 (package! py-yapf)
 (package! py-isort)
+
+;;;;;;;;;;;
+;; Julia ;;
+;;;;;;;;;;;
+
+(package! ob-julia :recipe
+  (:host github
+   :repo "phrb/ob-julia"
+   :files ("*.el")))
